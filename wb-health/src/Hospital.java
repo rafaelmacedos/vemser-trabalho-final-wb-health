76e453a82de76b7cd823a78df00deb193f330ab1
Rafael Macedo <macedorafael2002@gmail.com>
2023-07-17 21:42:13 -0300

    feat: Estrutura Classe Hospital e correções do projeto e pdf

---
+++ wb-health/src/Hospital.java
@@ -0,0 +1,9 @@
+public class Hospital {
+    public String cnpj;
+    public String nome;
+    public Integer numeroLeitos;
+    public Integer leitosOcupados;
+    public Medico[] medicos;
+    public Paciente[] pacientes;
+    public Atendimento[] atendimentos;
+}
